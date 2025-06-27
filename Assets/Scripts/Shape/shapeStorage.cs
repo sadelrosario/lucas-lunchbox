b862b10a47ac5c09d78471f1174dfad54b71b1a2
fmgaring <fritzmgaring@gmail.com>
2025-06-27 15:41:39 +0800

    Somehow managed to add food images to the shapes (WIP)

--- Assets/Scripts/Shape/shapeStorage.cs
+++ Assets/Scripts/Shape/shapeStorage.cs
@@ -5,12 +5,15 @@ public class shapeStorage : MonoBehaviour
 {
     public List<shapeData> shapeDataList;
     public List<shape> shapeList;
+
+    int i = 0;
     void Start()
     {
         foreach (var shape in shapeList)
         {
-            var shapeIndex = UnityEngine.Random.Range(0, shapeDataList.Count);
-            shape.CreateShape(shapeDataList[shapeIndex]);
+            //var shapeIndex = UnityEngine.Random.Range(0, shapeDataList.Count);
+            shape.CreateShape(shapeDataList[i]);
+            i++;
         }
     }
 
