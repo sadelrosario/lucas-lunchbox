4ce8f4236e9ef5e529cd9fac285d6b55bf18b8ec
sadelrosario <sherrie.delrosario@student.ateneo.edu>
2025-07-07 15:00:51 +0800

    debugged

--- Assets/Scripts/Shape/shapeData.cs
+++ Assets/Scripts/Shape/shapeData.cs
@@ -1,233 +1,63 @@
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using System;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.U2D.IK;
 using UnityEngine.UIElements;
 
-public class shape : MonoBehaviour, IPointerClickHandler, IPointerUpHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler
+[CreateAssetMenu]
+[System.Serializable]
+public class shapeData : ScriptableObject
 {
-    public GameObject squareShapeImage;
-    public Vector3 shapeSelectedScale;
-    public Vector2 offset = new Vector2(0f, 700f);
-
-    [HideInInspector]
-    public shapeData currentShapeData;
-
-    private List<GameObject> _currentShape = new List<GameObject>();
-    private Vector3 _shapeStartScale;
-    private RectTransform _transform;
-    private bool _shapeDraggable = true;
-    private Canvas _canvas;
-
-    public void Awake()
+    [System.Serializable]
+    public class Row
     {
-        _shapeStartScale = this.GetComponent<RectTransform>().localScale;
-        _transform = this.GetComponent<RectTransform>();
-        _canvas = GetComponentInParent<Canvas>();
-        _shapeDraggable = true;
-    }
-
-    public void RequestNewShape(shapeData sd)
-    {
-        CreateShape(sd);
-    }
-
-    public void CreateShape(shapeData sd)
-    {
-        currentShapeData = sd;
-        var totalSquareNumber = GetNumberOfSquares(sd);
-
-        while (_currentShape.Count <= totalSquareNumber)
+        // A row is comprised of columns
+        public bool[] column;
+        private int _size = 0;
+        public Row() { }
+        public Row(int size)
         {
-            _currentShape.Add(Instantiate(squareShapeImage, transform) as GameObject);
+            CreateRow(size);
         }
 
-        foreach (var square in _currentShape)
+        public void CreateRow(int size)
         {
-            square.gameObject.transform.position = Vector3.zero;
-            square.gameObject.SetActive(false);
+            // A row contains n columns that are n booleans. All are set to false upon instantiation.
+            _size = size;
+            column = new bool[_size];
+            ClearRow();
         }
 
-        var squareRect = squareShapeImage.GetComponent<RectTransform>();
-        var moveDistance = new Vector2(squareRect.rect.width * squareRect.localScale.x, squareRect.rect.height * squareRect.localScale.y);
-
-        int currentIndexInList = 0;
-
-        for (var row = 0; row < sd.rows; row++)
+        public void ClearRow()
         {
-            for (var col = 0; col < sd.columns; col++)
+            for (int i = 0; i < _size; i++)
             {
-                if (sd.board[row].column[col])
-                {
-                    _currentShape[currentIndexInList].SetActive(true);
-                    _currentShape[currentIndexInList].GetComponent<RectTransform>().localPosition = new Vector2(GetXPosShapeSquare(sd, col, moveDistance), GetYPosShapeSquare(sd, row, moveDistance));
-                    currentIndexInList++;
-                }
+                column[i] = false;
             }
         }
     }
 
-    private float GetYPosShapeSquare(shapeData sd, int row, Vector2 moveDistance)
-    {
-        float shiftOnY = 0f;
+    // The following three fields comprise the "Shape Data": 
+    // Firstly, the numbers of rows and columns
+    // Then, a board of Rows. As Rows contain the columns, a group of Rows make up the "Shape Board".
+    // Each Row in the board is cleared upon instantiation.
 
-        if (sd.rows > 1)
+    public int columns = 0;
+    public int rows = 0;
+    public Row[] board;
+    public void Clear()
+    {
+        for (var i = 0; i < rows; i++)
         {
-            if (sd.rows % 2 != 0)
-            {
-                var middleSquareIndex = (sd.rows - 1) / 2;
-                var multiplier = (sd.rows - 1) / 2;
-                if (row < middleSquareIndex)
-                {
-                    shiftOnY = moveDistance.y * 1;
-                    shiftOnY *= multiplier;
-                }
-                else if (row > middleSquareIndex)
-                {
-                    shiftOnY = moveDistance.y * -1;
-                    shiftOnY *= multiplier;
-                }
-            }
-            else
-            {
-                var middleSquareIndex2 = (sd.rows == 2) ? 1 : (sd.rows / 2);
-                var middleSquareIndex1 = (sd.rows == 2) ? 0 : (sd.rows - 2);
-                var multiplier = sd.rows / 2;
-
-                if (row == middleSquareIndex1 || row == middleSquareIndex2)
-                {
-                    if (row == middleSquareIndex2)
-                    {
-                        shiftOnY = (moveDistance.y / 2) * -1;
-                    }
-                    if (row == middleSquareIndex1)
-                    {
-                        shiftOnY = (moveDistance.y / 2);
-                    }
-                }
-
-                if (row < middleSquareIndex1 && row < middleSquareIndex2)
-                {
-                    shiftOnY = moveDistance.y * 1;
-                    shiftOnY *= multiplier;
-                }
-                else if (row > middleSquareIndex1 && row > middleSquareIndex2)
-                {
-                    shiftOnY = moveDistance.y * -1;
-                    shiftOnY *= multiplier;
-                }
-            }
+            board[i].ClearRow();
         }
-        return shiftOnY;
     }
 
-    private float GetXPosShapeSquare(shapeData sd, int column, Vector2 moveDistance)
+    // Create a board by creating n rows of m columns.
+    public void CreateNewBoard()
     {
-        float shiftOnX = 0f;
-
-        if (sd.columns > 1)
+        board = new Row[rows];
+        for (var i = 0; i < rows; i++)
         {
-            if (sd.columns % 2 != 0)
-            {
-                var middleSquareIndex = (sd.columns - 1) / 2;
-                var multiplier = (sd.columns - 1) / 2;
-                if (column < middleSquareIndex)
-                {
-                    shiftOnX = moveDistance.x * -1;
-                    shiftOnX *= multiplier;
-                }
-                else if (column > middleSquareIndex)
-                {
-                    shiftOnX = moveDistance.x * 1;
-                    shiftOnX *= multiplier;
-                }
-            }
-            else
-            {
-                var middleSquareIndex2 = (sd.columns == 2) ? 1 : (sd.columns / 2);
-                var middleSquareIndex1 = (sd.columns == 2) ? 0 : (sd.columns - 1);
-                var multiplier = sd.columns / 2;
-
-                if (column == middleSquareIndex1 || column == middleSquareIndex2)
-                {
-                    if (column == middleSquareIndex2)
-                    {
-                        shiftOnX = moveDistance.x / 2;
-                    }
-                    if (column == middleSquareIndex1)
-                    {
-                        shiftOnX = moveDistance.x / 2 * -1;
-                    }
-                }
-
-                if (column < middleSquareIndex1 && column < middleSquareIndex2)
-                {
-                    shiftOnX = moveDistance.x * -1;
-                    shiftOnX *= multiplier;
-                }
-                else if (column > middleSquareIndex1 && column > middleSquareIndex2)
-                {
-                    shiftOnX = moveDistance.x * 1;
-                    shiftOnX *= multiplier;
-                }
-            }
+            board[i] = new Row(columns);
         }
-        return shiftOnX;
-    }
-
-    private int GetNumberOfSquares(shapeData sd)
-    {
-        int number = 0;
-
-        foreach (var row in sd.board)
-        {
-            foreach (var active in row.column)
-            {
-                if (active)
-                {
-                    number++;
-                }
-            }
-        }
-
-        return number;
-    }
-    public void OnPointerClick(PointerEventData eventData)
-    {
-
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        this.GetComponent<RectTransform>().localScale = shapeSelectedScale;
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        _transform.anchorMin = new Vector2(0, 0);
-        _transform.anchorMax = new Vector2(0, 0);
-        _transform.pivot = new Vector2(0, 0);
-
-        Vector2 pos;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvas.transform as RectTransform, eventData.position, Camera.main, out pos);
-        _transform.localPosition = pos + offset;
-
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        this.GetComponent<RectTransform>().localScale = _shapeStartScale;
-        GameEvents.CheckPlaceable();
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-
     }
 }
